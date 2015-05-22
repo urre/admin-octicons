8cd9053fe38f262a712fc7b680c5bd0fd0dc1715
Urban Sanden <hej@urre.me>
2015-05-23 00:21:04 +0200

    Cleanup

--- admin-octicons.php
+++ admin-octicons.php
@@ -33,53 +33,11 @@ class AdminOcticons {
         # Register admin styles and scripts
         add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
 
-        add_action( 'init', array( $this, 'register_case' ) );
-
     }
 
     public function register_admin_styles() {
 
          wp_enqueue_style( 'AdminOcticons-plugin-styles', plugins_url( 'admin-octicons/bower_components/octicons/octicons/octicons.css' ) );
-
-    }
-
-    public function register_case() {
-
-        register_post_type( 'case',
-           array(
-               'capability_type'     => 'post',
-               'hierarchical'        => false,
-               'public'              => true,
-               'show_ui'             => true,
-               'publicly_queryable'  => true,
-               'exclude_from_search' => false,
-               'menu_position'       => 4,
-               'labels' => array(
-                   'name'               => __( 'Github Issues' ),
-                   'singular_name'      => __( 'Case' ),
-                   'add_new'            => __( 'Nytt case' ),
-                   'add_new_item'       => __( 'Nytt case' ),
-                   'edit'               => __( 'Redigera case' ),
-                   'edit_item'          => __( 'Redigera case' ),
-                   'new_item'           => __( 'Nytt case' ),
-                   'view'               => __( 'Visa case' ),
-                   'view_item'          => __( 'Visa case' ),
-                   'search_items'       => __( 'Sök case' ),
-                   'not_found'          => __( 'Inga case hittades' ),
-                   'not_found_in_trash' => __( 'Inga case hittades i papperskorgen' )
-               ),
-               'has_archive' => true, // Use custom page template here, due to same name slug
-               'supports' => array('title', 'revisions', 'editor', 'thumbnail'),
-               'query_var' => true,
-               // this sets where the Personaler section lives and contains a tag to insert the Cuisine in URL below
-               // this can be any depth e.g. food/cooking/case/%case_type%
-               'rewrite' => array( 'slug' => 'case', 'with_front' => false ),
-               'menu_icon' => plugins_url('admin-octicons/bower_components/octicons/svg/octoface.svg')
-           )
-        );
-
-
-
 
     }
 
